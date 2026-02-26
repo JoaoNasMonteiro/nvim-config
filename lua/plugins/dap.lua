6cf41a594f2fbdde53dd39be7b735687acdb1bd9
Joao Pedro Nascimento Monteiro <joaonasmont@gmail.com>
2026-02-26 13:22:20 -0300

    Checkhealth fixes

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,10 +3,10 @@ return {
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
-    
+
     "williamboman/mason.nvim",
     "jay-babu/mason-nvim-dap.nvim",
-    
+
     "mfussenegger/nvim-dap-python",
   },
   config = function()
@@ -18,8 +18,17 @@ return {
         "codelldb",
         "python",
       },
-      handlers = {}, 
+      handlers = {},
     })
+
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+        args = { "--port", "${port}" },
+      }
+    }
 
     dapui.setup()
 
@@ -37,13 +46,9 @@ return {
     require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
 
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Alternar Breakpoint' })
-    
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Iniciar/Continuar' })
-    
     vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Pular Linha' })
-    
     vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debug: Entrar na Função' })
-    
     vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Sair da Função' })
   end,
 }
