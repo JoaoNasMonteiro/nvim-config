70a752e5f771eb1ec8c2b7731dc9bc9147c03b48
Nagô (JJ) <joaonasmont@gmail.com>
2026-02-27 11:38:03 -0300

    Add key mappings for paste and delete actions
    Added key mappings for paste and delete without copying.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,10 +12,9 @@ map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Aumentar largura da j
 
 map("n", "<leader>q", ":q<CR>", { desc = "Fechar janela" })
 
+map("v", "p", '"_dP', { desc = "Paste without overwriting clipboard" })
+map({"n", "v"}, "x", '"_x', { desc = "Delete character without copying" })
+
 -- Terminal mode stuffies
 map("t", "<C-Q>", "<C-\\><C-n>", { desc = "Sair do modo terminal" })
 
--- map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Ir para a janela da esquerda" })
--- map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Ir para a janela de baixo" })
--- map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Ir para a janela de cima" })
--- map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Ir para a janela da direita" })
