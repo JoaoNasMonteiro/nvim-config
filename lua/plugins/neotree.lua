b4d5da89bd028ccfa4b263b79758eac9ca68c59e
Joao Pedro Nascimento Monteiro <joao.monteiro@gmail.com>
2026-02-27 16:25:24 -0300

    remove some comments

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,21 +3,21 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",        
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
   },
   config = function()
     require("neo-tree").setup({
-      close_if_last_window = true, 
-      
+      close_if_last_window = true,
+
       filesystem = {
         filtered_items = {
-          visible = false, 
-          hide_dotfiles = false, -- Permite ver arquivos como .gitignore ou .config
-          hide_gitignored = true, -- Esconde arquivos ignorados pelo git (ex: pastas build/)
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = true,
         },
       },
-      
+
       window = {
         position = "left",
         width = 30,
@@ -25,25 +25,22 @@ return {
           noremap = true,
           nowait = true,
         },
-        -- Aqui definimos como navegar dentro da árvore
         mappings = {
-          ["<space>"] = "none", -- Desativa o espaço para não conflitar com a nossa tecla leader
-          ["l"] = "open",       -- Aperte 'l' para abrir uma pasta ou arquivo
-          ["h"] = "close_node", -- Aperte 'h' para fechar uma pasta
-          ["a"] = { 
+          ["<space>"] = "none",
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["a"] = {
             "add",
-            -- Este comando configura o neo-tree para perguntar qual o nome do arquivo a ser criado
             config = {
-              show_path = "none" 
+              show_path = "none"
             }
           },
-          ["d"] = "delete",     -- Aperte 'd' para deletar o arquivo sob o cursor
-          ["r"] = "rename",     -- Aperte 'r' para renomear
+          ["d"] = "delete",
+          ["r"] = "rename",
         }
       }
     })
 
-    -- Atalho principal: Espaço + e (Explorer) abre e fecha a barra lateral
     vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Alternar Explorador de Arquivos', silent = true })
   end,
 }
