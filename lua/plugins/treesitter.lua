cdfbab592bfa3b3113d35da3f4de22daa9da2c9b
JJP <joaonasmont@gmail.com>
2026-07-09 19:40:17 -0300

    aa

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		local ts = require("nvim-treesitter")
 
+		-- 1. Instalação manual dos parsers que você definiu
 		ts.install({
 			"c",
 			"cpp",
@@ -18,17 +19,22 @@ return {
 			"vimdoc",
 			"query",
 			"make",
-			"latex",
 			"ledger",
 			"bash",
 			"asm",
 		})
 
+		-- 2. Autocmd para ativar o Tree-sitter respeitando a regra do LaTeX
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "*",
 			callback = function(args)
-				local ok = pcall(vim.treesitter.start, args.buf)
+				-- Se o arquivo for LaTeX, aborta e deixa o VimTeX trabalhar sozinho
+				if vim.bo[args.buf].filetype == "tex" or vim.bo[args.buf].filetype == "latex" then
+					return
+				end
 
+				-- Ativa o Tree-sitter nos outros tipos de arquivo
+				local ok = pcall(vim.treesitter.start, args.buf)
 				if ok then
 					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 				end
---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,15 @@
+-- lua/plugins/vimtex.lua
+return {
+	"lervag/vimtex",
+	lazy = false,
+	init = function()
+		vim.g.vimtex_view_method = "zathura"
+
+		vim.g.vimtex_compiler_method = "latexmk"
+
+		vim.g.vimtex_quickfix_ignore_filters = {
+			"Underfull \\hbox",
+			"Overfull \\hbox",
+		}
+	end,
+}
